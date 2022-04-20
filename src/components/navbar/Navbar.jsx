7f22e4085bc3bc1fa7725b0ad3ac5b6280e2f263
Fificode <lamidifirdaws@gmail.com>
2022-04-20 01:09:38 +0100

    first commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
+import  { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../images/logo.png'
 
+const Menu = () => (
+  <>
+  <li><a href="#home">Home</a></li>
+<li><a href="#rooms">Rooms</a></li>
+<li><a href="#About">About</a></li>
+  </>
+)
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className='tanners__navbar'>
       <div className="tanners__navbar-links_logo">
@@ -16,6 +26,25 @@ const Navbar = () => {
 <div className="tanners__navbar-sign">
   <button className='tanners__navbar-login'>Log In</button>
   <button className='tanners__navbar-signup'>Sign Up</button>
+</div>
+<div className="tanners__navbar-menu">
+{toggleMenu? 
+<RiCloseLine color='#fff' size={27} onClick = {() => setToggleMenu(false)} />:
+<RiMenu3Line color='#fff' size={27} onClick = {() => setToggleMenu(true)} />
+}
+{
+  toggleMenu && (
+    <div className="tanners__navbar-menu_container scale-up-center ">
+      <ul className='tanners__navbar-menu_container-links'>
+<Menu />
+      </ul>
+      <div className="tanners__navbar-menu_container-links_sign">
+        <button className='tanners__navbar-menu_login'>Log In</button>
+  <button className='tanners__navbar-menu_signup'>Sign Up</button>
+      </div>
+    </div>
+  )
+}
 </div>
     </div>
   )

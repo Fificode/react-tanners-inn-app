7c421f79feade69ab4a1d8a8cbef7aa5efb1e88c
Fificode <lamidifirdaws@gmail.com>
2022-04-23 23:13:20 +0100

    first commit

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import './header.css';
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="tanners__header">
+      <div className="tanners__header-content">
+        <h1>TANNER'S</h1>
+        <h2>INN</h2>
+        <p>Experience comfort, grand hospitality like never before and have your cares and needs taken care of by our staff.
+</p>
+<p>We are a brand that puts the customers before ourselves in order to be able to deliver excellent all-round service.</p>
+<h3>Eat. Wine. Dine. Exercise. Rest. </h3>
+      </div>
+    </div>
   )
 }
 

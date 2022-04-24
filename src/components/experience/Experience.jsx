edbd616240e124ca8f12fe7c5d994ae3f02c270c
Fificode <lamidifirdaws@gmail.com>
2022-04-24 23:26:30 +0100

    first commit

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './experience.css';
 
+
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div className="tanners__experience">
+      <div className="tanners__experience-images_container">
+        <div className="tanners__experience-image_container-one">
+         <div className="tanners__experience-dark_background">
+           <span>Food</span>
+         </div>  
+        </div> 
+        <div className="tanners__experience-image_container-two">
+          <div className="tanners__experience-dark_background">
+          <span>Yoga</span>
+          </div>
+        </div>
+        <div className="tanners__experience-image_container-three">
+           <div className="tanners__experience-dark_background">
+          <span>Room service</span>
+          </div>
+        </div>
+        </div>
+      </div>
+    
   )
 }
 
--- src/containers/aboutus/AboutUs.jsx
+++ src/containers/aboutus/AboutUs.jsx
@@ -3,8 +3,13 @@ import './aboutus.css';
 import {Experience, About} from '../../components'
 const AboutUs = () => {
   return (
-    <div>
+    <div className='tanners__aboutus'>
+      <div className="tanners__experience">
+      <div className="tanners__experience-container">
+        <h3>EXPERIENCE</h3>
       <Experience />
+      </div>
+      </div>
       <About />
     </div>
   )

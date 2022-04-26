0d9435f373c5f37e0e75deba8758a8121e1aead2
Fificode <lamidifirdaws@gmail.com>
2022-04-27 00:19:39 +0100

    first commit

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,6 +3,25 @@ import './experience.css';
 
 
 const Experience = () => {
+  let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("tanners__experience-carousel_dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); 
+}
   return (
     <>
     <div className="tanners__experience">
@@ -34,6 +53,40 @@ const Experience = () => {
 </div>
         </div> 
       </div>
+    <div className="tanners__experience-carousel">
+      <div className="tanners__experience-carousel_images-container">
+        <div className='tanners__experience-carousel_overall-container mySlides fade'>
+        <div className="tanners__experience-carousel_image-container_one">
+         <div className="tanners__experience-carousel_dark-background">
+         <div className="tanners__experience-carousel_content"><span>Food</span></div>  
+         </div>  
+        </div> 
+        </div>
+        <div className='tanners__experience-carousel_overall-container mySlides fade'>
+        <div className="tanners__experience-carousel_image-container_two">
+          <div className="tanners__experience-carousel_dark-background">
+            <div className="tanners__experience-carousel_content">
+          <span>Yoga</span>
+          </div>
+          </div>
+        </div>
+</div>
+<div className='tanners__experience-carousel_overall-container mySlides fade'>
+        <div className="tanners__experience-carousel_image-container_three">
+           <div className="tanners__experience-carousel_dark-background">
+             <div className="tanners__experience-carousel_content">
+          <span>Room service</span>
+          </div>
+          </div>
+        </div>
+</div>
+        </div> 
+      </div>
+      <div className="tanners__experience-carousel_dot-container">
+  <span class="tanners__experience-carousel_dot"></span> 
+  <span class="tanners__experience-carousel_dot"></span> 
+  <span class="tanners__experience-carousel_dot"></span> 
+</div>
     </>
   )
 }
